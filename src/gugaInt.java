f0ea46f7da5c04e1e8a705eda787b4696a3285dd
gugatasci <gugatasci@hotmail.com>
2024-11-05 18:23:05 +0100

    new

---
+++ src/gugaInt.java
@@ -0,0 +1,27 @@
+public class gugaInt {
+
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        int numbernext = number +1;
+
+        System.out.println(number);
+        System.out.println(numbernext);
+
+        numbernext = numbernext*2;
+
+
+        System.out.println("Updated");
+        System.out.println(numbernext);
+
+        numbernext++;
+
+        System.out.println(numbernext);
+
+
+
+
+
+    }
+}
